4fc8148d3749abb09464b1ee1c7e67a89d060289
John <pickleman792@gmail.com>
2024-03-22 14:07:59 -0400

    quick update

--- utility/commit.py
+++ utility/commit.py
@@ -1,5 +1,12 @@
 import os
+import sys
+if(len(sys.argv)>1):
+    if(sys.argv[1] == "-q"):
+        msg = "quick update"
+if(len(sys.argv) == 1):
+    msg = input("provide commit msg: ")
+
+
 os.system("git add --all")
-msg = input("provide commit msg: ")
 os.system(f"git commit -m {msg}")
 os.system("git push")
