28965e45122c28f6b51733cbb1f29692755e3250
John <pickleman792@gmail.com>
2024-03-22 13:54:47 -0400

    toml

---
+++ target/debug/deps/rusty_repo-13a04c8acd4cc982.d
@@ -0,0 +1,5 @@
+/home/lando/Desktop/Source/rust/rusty_repo/target/debug/deps/rusty_repo-13a04c8acd4cc982: src/main.rs
+
+/home/lando/Desktop/Source/rust/rusty_repo/target/debug/deps/rusty_repo-13a04c8acd4cc982.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rusty_repo.d
@@ -0,0 +1,1 @@
+/home/lando/Desktop/Source/rust/rusty_repo/target/debug/rusty_repo: /home/lando/Desktop/Source/rust/rusty_repo/src/main.rs
