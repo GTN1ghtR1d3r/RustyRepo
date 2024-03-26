937405bb2f6cac26cf9e427f9cf9085845412efc
John <pickleman792@gmail.com>
2024-03-26 14:33:38 -0400

    playing with multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
-use iced::widget::text;
-use iced::{Element, Sandbox, Settings};
-
-fn main() -> iced::Result{
-    Editor::run(Settings::default())
-}
-
-struct Editor;
-
-#[derive(Debug)]
-enum Message{}
-
-impl Sandbox for Editor{
-    type Message = Message;
-
-    fn new() -> Self{
-        Self
-    }
-    fn title(&self) -> String{
-        String::from("A cool editor!")
-    }
-    fn update(&mut self, message: Message){
-        match message {}
-    }
-    fn view(&self) -> Element<'_, Message>{
-        text("Hello, iced!").into()
-    }
+pub mod my_class;
+use crate::my_class::BaseFunc;
+fn main(){
+    let instance = my_class::MyClass{a:17.0, b:22.0};
+    let result1 = instance.mult();
+    let result2 = instance.div();
+    println!("multiply results in {result1}");
+    println!("div results in {result2}");
 }
